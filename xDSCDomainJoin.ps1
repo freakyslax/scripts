04db89f83a02c2174ce88aa26f398432d3f69e50
freakyslax <boudondonne@yahoo.com>
2020-04-27 20:01:31 -0600

    Add files via upload

---
+++ xDSCDomainJoin.ps1
@@ -0,0 +1,29 @@
+﻿ Configuration DomainJoinConfiguration
+{   
+    Import-DscResource -ModuleName 'xDSCDomainjoin'   
+    $secdomainpasswd = ConvertTo-SecureString "u3n07SK0%anU" -AsPlainText -Force
+    $mydomaincreds = New-Object System.Management.Automation.PSCredential("adjoin@hq.standardoptical.net", $secdomainpasswd)
+        
+    node $AllNodes.NodeName   
+    {
+        xDSCDomainjoin JoinDomain
+        {
+            Domain = 'hq.standardoptical.net'
+            Credential = $mydomaincreds
+            JoinOU = "OU=RDS Servers,OU=Azure,OU=Servers,DC=hq,DC=standardoptical,DC=net"
+        }
+    }
+}
+#$ConfigData = @{
+#    AllNodes = @(
+#        @{
+#            NodeName = "*"
+#            PSDscAllowPlainTextPassword = $True
+#            PSDscAllowDomainUser = $true
+#           
+#        }
+#        @{
+#            NodeName = "DomainJoined"
+#        }
+#    )
+#}
